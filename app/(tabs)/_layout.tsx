046623e2529af9f2ee58e9f1e3b9db0f8a15a63d
Bryan Kang <kjhbryan@gmail.com>
2023-11-03 11:40:11 +0800

    Added Tabs, prettier and eslint configs.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,9 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import {
+  SimpleLineIcons,
+  FontAwesome,
+  MaterialCommunityIcons,
+  AntDesign,
+} from '@expo/vector-icons';
 import { Link, Tabs } from 'expo-router';
 import { Pressable, useColorScheme } from 'react-native';
 
@@ -14,40 +19,96 @@ function TabBarIcon(props: {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
+function TabBarSimpleLineIcon(props: {
+  name: React.ComponentProps<typeof SimpleLineIcons>['name'];
+  color: string;
+}) {
+  return <SimpleLineIcons size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+function TabBarMaterialCommunityIcon(props: {
+  name: React.ComponentProps<typeof MaterialCommunityIcons>['name'];
+  color: string;
+}) {
+  return (
+    <MaterialCommunityIcons size={28} style={{ marginBottom: -3 }} {...props} />
+  );
+}
+
+function TabBarAntDesignIcon(props: {
+  name: React.ComponentProps<typeof AntDesign>['name'];
+  color: string;
+}) {
+  return <AntDesign size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+        tabBarActiveTintColor:
+          Colors[colorScheme ?? 'light'].tabBackgroundColor,
+        tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tabIconDefault,
+        tabBarActiveBackgroundColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarInactiveBackgroundColor:
+          Colors[colorScheme ?? 'light'].tabBackgroundColor,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 65,
+        },
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name='index'
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name='home' color={color} />,
+          // headerRight: () => (
+          //   <Link href='/modal' asChild>
+          //     <Pressable>
+          //       {({ pressed }) => (
+          //         <FontAwesome
+          //           name='info-circle'
+          //           size={25}
+          //           color={Colors[colorScheme ?? 'light'].text}
+          //           style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+          //         />
+          //       )}
+          //     </Pressable>
+          //   </Link>
+          // ),
+        }}
+      />
+      <Tabs.Screen
+        name='notes'
+        options={{
+          title: 'Lecture Notes',
+          tabBarIcon: ({ color }) => (
+            <TabBarSimpleLineIcon name='notebook' color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name='flashcard'
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Flash Card',
+          tabBarIcon: ({ color }) => (
+            <TabBarMaterialCommunityIcon
+              name='card-multiple-outline'
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <TabBarAntDesignIcon name='user' color={color} />
+          ),
         }}
       />
     </Tabs>
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,31 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
+const primaryColor = '#FFBEAC';
+const white = '#FFFFFF';
+
 export default {
   light: {
     text: '#000',
     background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    tint: white,
+    tabIconDefault: '#E4E4E4',
+    tabIconSelected: white,
+    tabBackgroundColor: primaryColor,
   },
+  // light: {
+  //   text: '#000',
+  //   background: '#fff',
+  //   tint: tintColorLight,
+  //   tabIconDefault: '#ccc',
+  //   tabIconSelected: tintColorLight,
+  // },
   dark: {
     text: '#fff',
     background: '#000',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
+    tabBackgroundColor: white,
   },
 };
