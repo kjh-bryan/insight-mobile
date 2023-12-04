715b824d04e0060b37277b433e3c129e7f0932b9
Bryan Kang <kjhbryan@gmail.com>
2023-12-05 02:21:39 +0800

    added hostedUrl

--- services/client.ts
+++ services/client.ts
@@ -5,7 +5,8 @@ const mobileHotspotUrl = '192.168.43.119';
 const defaultUrl = 'localhost';
 const homeUrl = '192.168.68.67';
 const outsideUrl = '192.168.1.80';
+const hostedURL = 'insight-sg-backend.azurewebsites.net';
 
-const client = axios.create({ baseURL: `http://${outsideUrl}:8080` });
+const client = axios.create({ baseURL: `http://${hostedURL}` });
 
 export default client;
