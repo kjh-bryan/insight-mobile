e2827f555c8db53367a7e8748b6fcf3bf64b6468
Bryan Kang <kjhbryan@gmail.com>
2023-12-06 09:16:27 +0800

    removed extra urls

--- services/client.ts
+++ services/client.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const expoUrl = '10.0.66.185';
-const mobileHotspotUrl = '192.168.43.119';
-const defaultUrl = 'localhost';
-const homeUrl = '192.168.68.67';
-const outsideUrl = '192.168.1.80';
 const hostedURL = 'insight-sg-backend.azurewebsites.net';
+
 const intialiseClient = async (status: string = 'normal') => {
   try {
     const client = axios.create({ baseURL: `https://${hostedURL}` });
